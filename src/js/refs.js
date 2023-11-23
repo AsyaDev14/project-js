96e69264e53873935b5f714ccda753e1f4b530f6
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-23 19:45:27 +0200

    function

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,6 @@
 const productsListEl = document.querySelector('#products-list .products-list');
 const popularListElement = document.querySelector('.popular-list');
+const cartCountSpan = document.querySelector('.header-cart-span')
 export const categorySelectEl = document.querySelector('#categories');
 export const productListEl = document.querySelector('.products-list');
 export const closeModalBtn = document.querySelector('[data-modal-close]');
@@ -7,4 +8,4 @@ export const modalEl = document.querySelector('[data-modal]');
 export const popularListEl = document.querySelector('.popular-list');
 export const discountProductsEl = document.querySelector('.discount-products');
 
-export default { productsListEl, popularListElement };
+export default { productsListEl, popularListElement, cartCountSpan };
