c73764d764c60c7bd9768ee18e5256bc3c0cd903
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-23 22:07:37 +0200

    just my commits

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,28 @@
-import storage from './storage.js';
+import load from './storage.js';
 import refs from './refs.js';
 
-function updateCartCount() {
-  const cartCount = localStorage.getItem('cartCount');
-  // Update the cart count in the span element
-  if (refs.cartCountSpan) {
-    refs.cartCountSpan.textContent = cartCount || '0';
-  }
+export const cartCount = [];
+
+// const button = document.querySelector('.test');
+// console.log('btn', button);
+// button.addEventListener('click', onButtonClick);
+
+// function onButtonClick() {
+//   cartCount.push(testItem);
+//   localStorage.setItem('localKey', JSON.stringify(cartCount))
+//   refs.cartCountSpan.textContent = cartCount.length;
+// };
+
+// localStorage.setItem('localKey', JSON.stringify(test))
+
+// get JSON from localStorage
+// const localCartCountJson = localStorage.getItem('localKey');
+const localCartCount = load('localKey');
+
+// JSON => Array
+// const localCartCount = JSON.parse(localCartCountJson);
+
+if (localCartCount) {
+  localCartCount.forEach(element => cartCount.push(element));
+  refs.cartCountSpan.textContent = cartCount.length;
 }
