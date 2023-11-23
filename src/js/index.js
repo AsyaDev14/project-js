7d32e9e9bb3fd0ccb2e329c1016557b674df9102
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-23 12:24:23 +0200

    add js

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import addProduct from './addProduct.js';
+import footer from './footer.js';
+import header from './header.js';
+import modal from './modal.js';
---
+++ src/js/storage.js
@@ -0,0 +1,28 @@
+function save(key, value){
+  try {
+    const serializedState = JSON.stringify(value)
+    localStorage.setItem(key, serializedState)
+  } catch (error) {
+    console.error("Set state error: ", error.message)
+  }
+}
+
+function load(key) {
+  try {
+    const serializedState = localStorage.getItem(key)
+    return serializedState === null ? undefined : JSON.parse(serializedState)
+  } catch (error) {
+    console.error("Get state error: ", error.message)
+    return null
+  }
+}
+
+function remove(key) {
+    localStorage.removeItem(key)
+}
+
+export default {
+  save,
+  load,
+  remove
+}
