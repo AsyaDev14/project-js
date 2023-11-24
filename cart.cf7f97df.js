053913197364fe3daf2cc1a387f8e35d761b21d2
AsyaDev14 <142207257+AsyaDev14@users.noreply.github.com>
2023-11-24 13:33:03 +0000

    Deploying to gh-pages from @ AsyaDev14/project-js@2a7c7d4d66d68d628ee5b3c9e1a5d60644c74d9e 🚀

---
+++ cart.cf7f97df.js
@@ -0,0 +1,54 @@
+!function(){function e(e,t,r,n){Object.defineProperty(e,t,{get:r,set:n,enumerable:!0,configurable:!0})}var t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},r={},n={},a=t.parcelRequired7c6;null==a&&((a=function(e){if(e in r)return r[e].exports;if(e in n){var t=n[e];delete n[e];var a={id:e,exports:{}};return r[e]=a,t.call(a.exports,a,a.exports),a.exports}var o=Error("Cannot find module '"+e+"'");throw o.code="MODULE_NOT_FOUND",o}).register=function(e,t){n[e]=t},t.parcelRequired7c6=a);var o=a.register;o("iE7OH",function(t,r){e(t.exports,"register",function(){return n},function(e){return n=e});var n,a=new Map;n=function(e,t){for(var r=0;r<t.length-1;r+=2)a.set(t[r],{baseUrl:e,path:t[r+1]})}}),o("aNJCr",function(t,r){e(t.exports,"getBundleURL",function(){return n},function(e){return n=e});var n,a={};n=function(e){var t=a[e];return t||(t=function(){try{throw Error()}catch(t){var e=(""+t.stack).match(/(https?|file|ftp|(chrome|moz|safari-web)-extension):\/\/[^)\n]+/g);if(e)return(""+e[2]).replace(/^((?:https?|file|ftp|(chrome|moz|safari-web)-extension):\/\/.+)\/[^/]+$/,"$1")+"/"}return"/"}(),a[e]=t),t}}),a("iE7OH").register(a("aNJCr").getBundleURL("86U9d"),JSON.parse('["86U9d","cart.cf7f97df.js","8AgqT","icons.1c31bae1.svg","djwkb","cart.e75d7ab4.js","6XgHV","cart.runtime.6d06fce3.js"]')),a("7hKzD"),a("i8Q71");let c=document.querySelector(".js-cart-span"),s=document.querySelector(".js-all-cart-content"),l=document.querySelector(".js-cart-list"),i=(document.querySelector(".js-cart-list"),document.querySelector(".js-order-container")),u=document.querySelector(".js-empty-cart");document.querySelector(".js-delete-all"),document.querySelectorAll(".js-delete"),document.querySelector(".js-success");var d={cartSpan:c,cartContent:s,productList:l,customerOrder:i,emptyCart:u};a("jzQFI");var p=a("81wSt"),f={};f=a("aNJCr").getBundleURL("86U9d")+"icons.1c31bae1.svg";var m=a("i8Q71");window.addEventListener("load",y);let b=new p.FoodBoutiqueAPI;function g({_id:e,name:t,img:r,category:n,size:a,price:o}){var c;return`
+        <li class="cart-list-item" data-product-id=${e}>
+            <img
+              class="product-img"
+              src="${r}"
+              alt="${t}"
+              loading='lazy'
+            />
+            <div class="product-info-container">
+              <h3 class="product-name">${t}</h3>
+              <div class="product-dscr">
+                <p class="product-category"><span class="notation">Category: </span>${n}</p>
+                <p class="product-size"><span class="notation"> Size: </span>${a}</p>
+              </div>
+              <p class="cart-price">$${o}</p>
+            </div>
+            <button class="delete-btn js-delete" type="button">
+              <svg class="" width="18" height="18">
+                <use href="${(c=f)&&c.__esModule?c.default:c}#icon-close"></use>
+              </svg>
+            </button>
+            <div class="counter-container">
+              <button class="counter-btn" type="button" data-action="decrement">
+                -
+              </button>
+              <span class="counter-value">0</span>
+              <button class="counter-btn" type="button" data-action="increment">
+                +
+              </button>
+            </div>
+        </li>
+    `}async function y(){if((0,m.updateCartFromStorage)(d.cartSpan),null===localStorage.getItem("localKey")){d.cartContent.classList.add("visually-hidden"),d.emptyCart.classList.remove("visually-hidden");return}let e=JSON.parse(localStorage.getItem("localKey"));try{let t=e.map(e=>b.fetchById(e)),r=await Promise.all(t);!function(e){let t=e.map(g).join("");d.productList.insertAdjacentHTML("beforeend",t)}(r),function(e){let t=function(e){let t=0;for(let r of e)t+=r.price;return t}(e);d.customerOrder.innerHTML=`
+        <h2 class="order-title">Your order</h2>
+        <div class="total-container">
+          <p class="sub-title">Total</p>
+          <p class="total-price">
+            <span class="notation">Sum: </span>$${t}
+          </p>
+        </div>
+        <form class="cart-form" name="Order">
+          <div class="input-container">
+            <label class="notation" for="email">Mail:</label>
+            <input
+              type="email"
+              id="email"
+              pattern=".+@example.com"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <button class="order-btn">Checkout</button>
+        </form>
+    `}(r)}catch(e){console.error(e)}}a("9JQa4")}();
+//# sourceMappingURL=cart.cf7f97df.js.map
---
+++ cart.d80eb3a3.js
@@ -0,0 +1,54 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t={},r={},n=e.parcelRequired7c6;null==n&&((n=function(e){if(e in t)return t[e].exports;if(e in r){var n=r[e];delete r[e];var o={id:e,exports:{}};return t[e]=o,n.call(o.exports,o,o.exports),o.exports}var a=Error("Cannot find module '"+e+"'");throw a.code="MODULE_NOT_FOUND",a}).register=function(e,t){r[e]=t},e.parcelRequired7c6=n);var o=n.register;o("kyEFX",function(e,t){Object.defineProperty(e.exports,"register",{get:function(){return r},set:function(e){return r=e},enumerable:!0,configurable:!0});var r,n=new Map;r=function(e,t){for(var r=0;r<t.length-1;r+=2)n.set(t[r],{baseUrl:e,path:t[r+1]})}}),o("2CmVG",function(e,t){e.exports=new URL("icons.1c31bae1.svg",import.meta.url).toString()}),n("kyEFX").register(new URL("",import.meta.url).toString(),JSON.parse('["d3pfZ","cart.d80eb3a3.js","2Gq7C","icons.1c31bae1.svg","28uIt","cart.ea51eff8.js","aIVm5","cart.runtime.3af2fe06.js"]')),n("epHO8"),n("bUb57");const a=document.querySelector(".js-cart-span"),c=document.querySelector(".js-all-cart-content"),s=document.querySelector(".js-cart-list"),l=(document.querySelector(".js-cart-list"),document.querySelector(".js-order-container")),i=document.querySelector(".js-empty-cart");document.querySelector(".js-delete-all"),document.querySelectorAll(".js-delete"),document.querySelector(".js-success");var u={cartSpan:a,cartContent:c,productList:s,customerOrder:l,emptyCart:i};n("5uEKZ");var d=n("fO53X"),p=n("2CmVG"),m=n("bUb57");window.addEventListener("load",y);const f=new d.FoodBoutiqueAPI;function b({_id:e,name:t,img:r,category:n,size:o,price:a}){return`
+        <li class="cart-list-item" data-product-id=${e}>
+            <img
+              class="product-img"
+              src="${r}"
+              alt="${t}"
+              loading='lazy'
+            />
+            <div class="product-info-container">
+              <h3 class="product-name">${t}</h3>
+              <div class="product-dscr">
+                <p class="product-category"><span class="notation">Category: </span>${n}</p>
+                <p class="product-size"><span class="notation"> Size: </span>${o}</p>
+              </div>
+              <p class="cart-price">$${a}</p>
+            </div>
+            <button class="delete-btn js-delete" type="button">
+              <svg class="" width="18" height="18">
+                <use href="${p&&p.__esModule?p.default:p}#icon-close"></use>
+              </svg>
+            </button>
+            <div class="counter-container">
+              <button class="counter-btn" type="button" data-action="decrement">
+                -
+              </button>
+              <span class="counter-value">0</span>
+              <button class="counter-btn" type="button" data-action="increment">
+                +
+              </button>
+            </div>
+        </li>
+    `}async function y(){if((0,m.updateCartFromStorage)(u.cartSpan),null===localStorage.getItem("localKey")){u.cartContent.classList.add("visually-hidden"),u.emptyCart.classList.remove("visually-hidden");return}let e=JSON.parse(localStorage.getItem("localKey"));try{let t=e.map(e=>f.fetchById(e)),r=await Promise.all(t);!function(e){let t=e.map(b).join("");u.productList.insertAdjacentHTML("beforeend",t)}(r),function(e){let t=function(e){let t=0;for(let r of e)t+=r.price;return t}(e);u.customerOrder.innerHTML=`
+        <h2 class="order-title">Your order</h2>
+        <div class="total-container">
+          <p class="sub-title">Total</p>
+          <p class="total-price">
+            <span class="notation">Sum: </span>$${t}
+          </p>
+        </div>
+        <form class="cart-form" name="Order">
+          <div class="input-container">
+            <label class="notation" for="email">Mail:</label>
+            <input
+              type="email"
+              id="email"
+              pattern=".+@example.com"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <button class="order-btn">Checkout</button>
+        </form>
+    `}(r)}catch(e){console.error(e)}}n("8ykkE");
+//# sourceMappingURL=cart.d80eb3a3.js.map
