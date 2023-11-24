0962b33d466cff4ad34a8af98de23472d65198b5
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-24 10:22:51 +0200

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,28 +1,26 @@
-import load from './storage.js';
-import refs from './refs.js';
+// import load from './storage.js';
+// import refs from './refs.js';
 
-export const cartCount = [];
-
-// const button = document.querySelector('.test');
-// console.log('btn', button);
-// button.addEventListener('click', onButtonClick);
-
-// function onButtonClick() {
-//   cartCount.push(testItem);
-//   localStorage.setItem('localKey', JSON.stringify(cartCount))
-//   refs.cartCountSpan.textContent = cartCount.length;
-// };
-
-// localStorage.setItem('localKey', JSON.stringify(test))
-
-// get JSON from localStorage
-// const localCartCountJson = localStorage.getItem('localKey');
-const localCartCount = load('localKey');
-
+// export const cartCount = [];
+// const localCartCount = load('localKey');
 // JSON => Array
 // const localCartCount = JSON.parse(localCartCountJson);
 
-if (localCartCount) {
-  localCartCount.forEach(element => cartCount.push(element));
-  refs.cartCountSpan.textContent = cartCount.length;
+// if (localCartCount) {
+//   localCartCount.forEach(element => cartCount.push(element));
+//   refs.cartCountSpan.textContent = cartCount.length;
+// }
+
+import refs from './refs.js';
+import Storage from './storage.js';
+
+const STORAGE_KEY = 'localKey';
+
+function updateCartFromStorage() {
+  const selectedProdIds = Storage.load(STORAGE_KEY) || [];
+  refs.cartCountSpan.textContent = selectedProdIds.length.toString();
 }
+
+window.addEventListener('load', updateCartFromStorage);
+
+export default updateCartFromStorage;
