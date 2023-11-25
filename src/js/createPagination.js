e416e7a9fe94db8c971f4055a0a0656620939fc6
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-26 00:44:26 +0100

    Update createPagination.js

--- src/js/createPagination.js
+++ src/js/createPagination.js
@@ -5,67 +5,23 @@ import axios from 'axios';
 import {renderProductsCards} from './productsList'
 import refs from './refs';
 
-// 1variant
-
-// const BASE_URL = 'https://food-boutique.b.goit.study/api';
-// const END_POINT = 'products'
-// const btnPagination = document.querySelector('#pagination');
-// const arrowPaginPrev = document.querySelector('.tui-prev');
-// const arrowPaginNext = document.querySelector('.tui-next');
-
-// const limit = 6;
-// let page = 1;
-
-// async function getPages(limit, page, category= "Pantry_Items") {
-//   try {
-//     return await axios.get(`${BASE_URL}/${END_POINT}`, {
-//       params: {
-//         limit: limit,
-//         page: page,
-//         category: category,
-//         byABC: true,
-//         byPrice: true,
-//         byPopularity: true,
-//       }
-//     })
-//   } catch (error) {
-//     console.log(error.code)
-//   }
-// }
-// document.addEventListener('DOMContentLoaded', renderPages);
-// btnPagination.addEventListener('click', onThisPage);
-
-// function renderPages() {
-//   getPages(limit, page)
-//     .then(res => {
-//       const { page, perPage, totalPages } = res.data
-//       const options = {
-//         totalItems: totalPages,
-//         itemsPerPage: perPage,
-//         visiblePages: 5,
-//         page: page,
-//       }
-//       const pagination = new Pagination('pagination', options);
-
-//       console.log(res.data)
-//     })
-//     .catch(err => console.log(err.message))
-// }
-
-// function onThisPage(ev) {
-//   page = ev.target.textContent;
-//   renderPages();
-// }
-
-// 2 variant
-
 const BASE_URL = 'https://food-boutique.b.goit.study/api';
 const END_POINT = 'products';
 
 let currentPage = 1;
 
-async function fetchPages(page, category = "Dairy") {
+refs.filtersFormSearchEL.addEventListener('submit', getCangeFromLocalStorage);
+
+function getCangeFromLocalStorage() {
+  const productsQuery = storage.load('productsQuery');
+  const { category } = productsQuery;
+  // fetchPages(1, category);
+  console.log(category)
+}
+
+async function fetchPages(page = 1, category = '') {
   try {
+
     return await axios.get(`${BASE_URL}/${END_POINT}`, {
       params: {
         limit: 6,
@@ -85,19 +41,13 @@ export function getProductsList() {
   fetchPages(currentPage)
     .then(res => {
       console.log(res.data);
-            const { page, perPage, totalPages, results } = res.data
-            renderProductsCards(results, refs.productsListEl)
+      const { page, perPage, totalPages, results } = res.data
+      renderProductsCards(results, refs.productsListEl)
       const optionsPagination = {
         totalItems: (totalPages * perPage),
         itemsPerPage: perPage,
         visiblePages: 4,
         page: page,
-        template: {
-          moreButton:
-            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-            '<span class="tui-ico-ellip">...</span>' +
-            '</a>'
-        }
       }
       const pagination = new Pagination('#pagination', optionsPagination);
 
