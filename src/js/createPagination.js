29f1a94d9c9901838b64d8db27a7f788eb5b199a
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-25 09:28:47 +0100

    pagination-local-js
    without render page

--- src/js/createPagination.js
+++ src/js/createPagination.js
@@ -1,13 +1,111 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+import { FoodBoutiqueAPI } from './foodBoutiqueApi';
+import axios from 'axios';
 
-const options = {
-  totalItems: 70,
-  itemsPerPage: 10,
-  visiblePages: 4,
-  page: 1,
-  centerAlign: false,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
+
+// 1variant
+
+// const BASE_URL = 'https://food-boutique.b.goit.study/api';
+// const END_POINT = 'products'
+// const btnPagination = document.querySelector('#pagination');
+// const arrowPaginPrev = document.querySelector('.tui-prev');
+// const arrowPaginNext = document.querySelector('.tui-next');
+
+// const limit = 6;
+// let page = 1;
+
+// async function getPages(limit, page, category= "Pantry_Items") {
+//   try {
+//     return await axios.get(`${BASE_URL}/${END_POINT}`, {
+//       params: {
+//         limit: limit,
+//         page: page,
+//         category: category,
+//         byABC: true,
+//         byPrice: true,
+//         byPopularity: true,
+//       }
+//     })
+//   } catch (error) {
+//     console.log(error.code)
+//   }
+// }
+// document.addEventListener('DOMContentLoaded', renderPages);
+// btnPagination.addEventListener('click', onThisPage);
+
+// function renderPages() {
+//   getPages(limit, page)
+//     .then(res => {
+//       const { page, perPage, totalPages } = res.data
+//       const options = {
+//         totalItems: totalPages,
+//         itemsPerPage: perPage,
+//         visiblePages: 5,
+//         page: page,
+//       }
+//       const pagination = new Pagination('pagination', options);
+
+//       console.log(res.data)
+//     })
+//     .catch(err => console.log(err.message))
+// }
+
+// function onThisPage(ev) {
+//   page = ev.target.textContent;
+//   renderPages();
+// }
+
+// 2 variant
+
+const BASE_URL = 'https://food-boutique.b.goit.study/api';
+const END_POINT = 'products';
+
+let currentPage = 1;
+
+async function fetchPages(page, category = "Pantry_Items") {
+  try {
+    return await axios.get(`${BASE_URL}/${END_POINT}`, {
+      params: {
+        limit: 6,
+        page: page,
+        category: category,
+        byABC: true,
+        byPrice: true,
+        byPopularity: true,
+      }
+    })
+  } catch (error) {
+    console.log(error.code)
+  }
 }
-const pagination = new Pagination('pagination', options);
+
+function getPages() {
+  fetchPages(currentPage)
+    .then(res => {
+      console.log(res.data);
+      const { page, perPage, totalPages, results } = res.data
+      const optionsPagination = {
+        totalItems: (totalPages * perPage),
+        itemsPerPage: perPage,
+        visiblePages: 5,
+        page: page,
+      }
+      const pagination = new Pagination('#pagination', optionsPagination);
+
+      pagination.on('beforeMove', (event) => {
+       currentPage = event.page;
+        getPages();
+        // function renderPages()
+        if (currentPage === totalPages) {
+          return false;
+        }
+      });
+
+    })
+    .catch(err => console.log(err.message))
+}
+getPages();
+
+
+
